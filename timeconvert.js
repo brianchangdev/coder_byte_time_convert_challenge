468aeba0ec49093d0b797d8ebdde1a4f1114af27
brianchangdev <brianchang.dev@gmail.com>
2016-06-22 23:14:44 -0700

    initial commit

---
+++ timeconvert.js
@@ -0,0 +1,20 @@
+
+
+/* Time Convert                                                                       
+Using the JavaScript language, have the function TimeConvert(num) take the num      
+parameter being passed and return the number of hours and minutes the parameter     
+converts to (ie. if num = 63 then the output should be 1:3). Separate the number    
+of hours and minutes with a colon. */                                               
+ 
+// steps to solve challenge
+// 1. convert num to hours as a whole number
+// 2. get minutes of the num by getting remainder of time after hour
+// 3. return output format of hours : minutes
+
+function timeConvert(num){
+  var hours = Math.floor(num / 60);
+  var minutes = num % 60;
+  return hours + ":" + minutes;
+
+}
+console.log(timeConvert(87));
